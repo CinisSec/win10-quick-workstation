a3d17fbff2abd0262bd045c2b72998b9b43a63c7
CinisSec <gregorypro@protonmail.com>
2017-10-04 23:31:09 +0200

    Adding autopsy download & install in forensics.ps1

--- forensics.ps1
+++ forensics.ps1
@@ -12,4 +12,9 @@ Packages: 	Sleuthkit
 
 #>
 
+$v_autopsy=autopsy-4.4.1
+
 choco install sleuthkit radare shellbagsexplorer timelineexplorer 
+wget https://github.com/sleuthkit/autopsy/releases/download/$v_autopsy/$v_autopsy-64bit.msi
+Start-Process msiexec.exe -ArgumentList "/I $v_autopsy-64bit.msi /quiet" -Wait
+rm $v_autopsy-64bit.msi
