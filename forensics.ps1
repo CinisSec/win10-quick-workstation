f21431d2e094c709645d37be0664179154a27a3d
CinisSec <gregorypro@protonmail.com>
2017-10-08 00:44:14 +0200

    Optimizing, removing cds

--- forensics.ps1
+++ forensics.ps1
@@ -2,6 +2,13 @@ $v_autopsy="autopsy-4.4.1"
 
 choco install sleuthkit radare shellbagsexplorer timelineexplorer -y
 #Downloading & installing Autopsy (msi)
+$choice= Read-Host -Prompt "Would you like to download $v_autopsy?(y/n)"
+switch ($choice)
+	{
+		'n' {"$v_autopsy won't be downloaded"}
+		'y' {"v_autopsy will be downloaded & stored in FOR_installers"}
+        default {"Error, couldn't define choice"}
+	}
 wget https://github.com/sleuthkit/autopsy/releases/download/$v_autopsy/$v_autopsy-64bit.msi
 Start-Process msiexec.exe -ArgumentList "/I $v_autopsy-64bit.msi /quiet" -Wait
 rm $v_autopsy-64bit.msi
@@ -16,7 +23,6 @@ switch ($choice)
         default {"Error, couldn't define choice"}
 	}
 mkdir FOR_installers
-cd FOR_installers
-wget http://www.nirsoft.net/utils/lastactivityview.zip
-wget http://mh-nexus.de/downloads/HxDSetupEN.zip
-wget https://www.paterva.com/malv41/maltego.JRE64_v4.1.0.10552.exe
+wget http://www.nirsoft.net/utils/lastactivityview.zip -O .\FOR_installers\lastactivityview.zip
+wget http://mh-nexus.de/downloads/HxDSetupEN.zip -O .\FOR_installers\HxDSetupEN.zip
+wget https://www.paterva.com/malv41/maltego.JRE64_v4.1.0.10552.exe -O .\FOR_installers\maltego.JRE64_v4.1.0.10552.exe
