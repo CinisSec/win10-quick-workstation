3b8177447881ebb30f21105eaac7d192cae89939
CinisSec <gregorypro@protonmail.com>
2017-10-08 00:12:18 +0200

    Moving key cli commands to coreutils

--- coreutils.ps1
+++ coreutils.ps1
@@ -1,1 +1,3 @@
 choco install git 7zip vim sudo openssh curl wget -y
+scoop install concfg pshazz coreutils file grep sed tar
+concfg import solarized small
--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,1 @@
 choco install notepadplusplus eclipse atom pycharm-community -y
-scoop install concfg pshazz
-concfg import solarized small
